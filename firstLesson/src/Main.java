83e5fb9b2b6bfc131663dcf9a97f55019c4b998a
MrHerbat <113918343+MrHerbat@users.noreply.github.com>
2023-09-06 14:45:31 +0200

    Add files via upload

--- firstLesson/src/Main.java
+++ firstLesson/src/Main.java
@@ -1,6 +1,55 @@
 public class Main {
     public static void main(String[] args)
     {
-        System.out.println("Hello world!");
+        boolean tf;
+        tf = true;
+        System.out.println(tf);
+        boolean tf2 = true;
+        char c = '$';
+        byte b = 2;
+        short s = 22;
+        int i = 22222;
+        long l = 2222222222222L;
+        float f = 5.8f;
+        double d = 4.56553d;
+        System.out.println("something");
+        System.out.println(c);
+        System.out.println(b);
+        System.out.println(s);
+        System.out.println(i);
+        System.out.println(l);
+        System.out.println(f);
+        System.out.println(d);
+
+
+        boolean birth = false;
+        System.out.print("Have I been born in first half of the year? ");
+        System.out.println(birth);
+        char fav = 'v';
+        System.out.print("My favorite letter is: ");
+        System.out.println(fav);
+        byte age = 12;
+        System.out.print("I was ");
+        System.out.print(age);
+        System.out.println(" when I got my first computer.");
+        short poland = 966;
+        System.out.print("Poland was baptized in ");
+        System.out.println(poland);
+        int population_then = 2000000000;
+        System.out.print("In 1930 world population was counting ");
+        System.out.println(population_then);
+        byte population_now = 8;
+        System.out.print("In 2022 world population was counting ");
+        System.out.print(population_now);
+        System.out.print(" billions!");
+        float e = 2.71828f;
+        System.out.print("Number 'e' is almost equal to ");
+        System.out.println(e);
+        double pi = 3.141592653589793238462643383279502884197169399375105820974944592307816406286208998628034825342117067982148086513282306647093844609550582231725359408128481117450284102701938521105559644622948954930381964D;
+        System.out.print("Pi in 200 digits: ");
+        System.out.println(pi);
+
+
+        /*233*/
     }
 }
