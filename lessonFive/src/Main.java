cc9708c8356fe571f314a2a4b0664da556e66deb
MrHerbat <113918343+MrHerbat@users.noreply.github.com>
2023-10-25 15:23:11 +0200

    Add files via upload

--- lessonFive/src/Main.java
+++ lessonFive/src/Main.java
@@ -12,7 +12,39 @@ public class Main
 
         Scanner scan = new Scanner(System.in);
         Car myCar = new Car();
-        Beetle smallBeetles = new Beetle();
+//        myCar.raceway();
+
+
+        System.out.print("How many turns there are?: ");
+        int turnsAmount = scan.nextInt();
+        int i = turnsAmount;
+        int[] b = new int[i];
+        int[] a = new int[i];
+        String[] turn = new String[i];
+        for(i = 0; i<turnsAmount;i++)
+        {
+            System.out.print("What is the distance until the turn?(meters): ");
+            a[i] = scan.nextInt();
+            System.out.print("In which direction car needs to turn?(left/right): ");
+            turn[i] = scan.next();
+            System.out.print("How sharp is next turn?: ");
+            b[i] = scan.nextInt();
+        }
+        for(i = 0; i<turnsAmount;i++)
+        {
+            myCar.forward(a[i]);
+            if(turn[i] == "left")
+            {
+                myCar.turnLeft(b[i]);
+            }
+            else
+            {
+                myCar.turnRight(b[i]);
+            }
+        }
+
+
+//        Beetle smallBeetles = new Beetle();
 
 //        System.out.print("Podaj swoje imię: ");
 //        String name = scan.nextLine();
@@ -24,33 +56,33 @@ public class Main
 //        int sum = a+b;
 //        System.out.println(a+" + "+b+" = "+sum);
 
-        System.out.print("Enter firstname: ");
-        String firstname = scan.nextLine();
-        System.out.print("Enter lastname: ");
-        String lastname = scan.nextLine();
-        System.out.print("Enter any char: ");
-        char character = scan.nextLine().charAt(0);
-        System.out.print("Enter any boolean: ");
-        boolean tf = scan.nextBoolean();
-        System.out.print("Enter any number: ");
-        int a = scan.nextInt();
-        System.out.print("Enter any decimal number(comma, not dot): ");
-        double db = scan.nextDouble();
-        System.out.print("Enter any word: ");
-        String word = scan.nextLine();
-        if(firstname != "" && lastname != "")
-        {
-            class Human
-            {
-                String[] name = {firstname,lastname};
-            }
-            Human human = new Human();
-            System.out.println(human.name[0] + " " + human.name[1]);
-        }
-        else
-        {
-            System.out.println("You didn't put firstname or lastname!");
-        }
+//        System.out.print("Enter firstname: ");
+//        String firstname = scan.nextLine();
+//        System.out.print("Enter lastname: ");
+//        String lastname = scan.nextLine();
+//        System.out.print("Enter any char: ");
+//        char character = scan.nextLine().charAt(0);
+//        System.out.print("Enter any boolean: ");
+//        boolean tf = scan.nextBoolean();
+//        System.out.print("Enter any number: ");
+//        int a = scan.nextInt();
+//        System.out.print("Enter any decimal number(comma, not dot): ");
+//        double db = scan.nextDouble();
+//        System.out.print("Enter any word: ");
+//        String word = scan.nextLine();
+//        if(firstname != "" && lastname != "")
+//        {
+//            class Human
+//            {
+//                String[] name = {firstname,lastname};
+//            }
+//            Human human = new Human();
+//            System.out.println(human.name[0] + " " + human.name[1]);
+//        }
+//        else
+//        {
+//            System.out.println("You didn't put firstname or lastname!");
+//        }
     }
 
 }
--- lessonFive/src/pojazdy/Car.java
+++ lessonFive/src/pojazdy/Car.java
@@ -1,11 +1,55 @@
 package pojazdy;
 
+import java.util.Scanner;
+
 public class Car
 {
-    String carColor;
+    double[] turnAngle;
+    String[] turnDirection;
+    int amountOfTurns;
+    double[] distanceBetweenTurns;
+
+    public void raceway()
+    {
+        Scanner scan = new Scanner(System.in);
+        System.out.print("How many turn there are in the raceway?(number) ");
+        amountOfTurns = scan.nextInt();
+        int i = amountOfTurns;
+        distanceBetweenTurns = new double[i];
+        turnDirection = new String[i];
+        turnAngle = new double[i];
+        for(i=0;i<amountOfTurns;i++)
+        {
+            System.out.print("Distance between turns(straight line): ");
+            distanceBetweenTurns[i] = scan.nextDouble();
+            System.out.print("Turn direction(left/right): ");
+            turnDirection[i] = scan.next();
+            System.out.print("Turn angle: ");
+            turnAngle[i] = scan.nextDouble();
+        }
+        for(i=0;i<amountOfTurns;i++)
+        {
+            System.out.println("car goes forward for "+distanceBetweenTurns[i]+" meters");
+            System.out.println("car turns "+turnDirection[i]+" by "+turnAngle[i]+" degrees");
+
+        }
+    }
+
+    public void turnRight(int a)
+    {
+        System.out.println("car turns right by "+a+" degrees");
+    }
+    public void turnLeft(int a)
+    {
+        System.out.println("car turns left by "+a+" degrees");
+    }
+    public void forward(int a)
+    {
+        System.out.println("car goes forward for "+a+" meters");
+    }
 
     public Car()
     {
-        this.carColor = carColor;
+
     }
 }
