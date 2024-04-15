3a52df3fcbbb57785234401074656f71ee920baf
Maciej <113918343+MrHerbat@users.noreply.github.com>
2024-04-15 11:33:26 +0200

    Update GamePanel.java

--- Gra/src/GameWindow/GamePanel.java
+++ Gra/src/GameWindow/GamePanel.java
@@ -8,23 +8,23 @@ public class GamePanel extends JPanel implements ChangeListener
     JPanel colorSelector;
     JSlider rSlider,gSlider,bSlider;
     JButton setColor;
-    Color currentColor, newColor;
+    Color currentColor, playerColor;
     private int r,g,b;
     public GamePanel()
     {
+        currentColor = new Color();
         colorSelector  = new JPanel();
         rSlider = new JSlider(0,255,0);
         rSlider.setPreferredSize(new Dimension(255,50));
+        rSlider.setOrientation(SwingConstants.HORIZONTAL);
         
         gSlider= new JSlider(0,255,0);
         gSlider.setPreferredSize(new Dimension(255,50));
+        gSlider.setOrientation(SwingConstants.HORIZONTAL);
         
         bSlider= new JSlider(0,255,0);
         bSlider.setPreferredSize(new Dimension(255,50));
-        
-        r = rSlider.getValue();
-        g = gSlider.getValue();
-        b = bSlider.getValue();
+        bSlider.setOrientation(SwingConstants.HORIZONTAL);
 
         colorSelector.setLayout(null);
         colorSelector.add(rSlider);
@@ -38,11 +38,15 @@ public class GamePanel extends JPanel implements ChangeListener
     {
         super.paintComponent(g);
         g.fillRect(100,150,100,100);
+        repaint();
     }
 
     @Override
     public void sateChanged(ChangeEvent e)
     {
-        
+        r = rSlider.getValue();
+        g = gSlider.getValue();
+        b = bSlider.getValue();
+        currentColor = new Color(r,g,b);
     }
 }
