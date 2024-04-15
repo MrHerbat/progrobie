d59afe6aa58c8bca77d9b7357be61e7dc6683ae8
Maciej <113918343+MrHerbat@users.noreply.github.com>
2024-04-15 11:23:18 +0200

    Update GamePanel.java

--- Gra/src/GameWindow/GamePanel.java
+++ Gra/src/GameWindow/GamePanel.java
@@ -3,7 +3,7 @@ package GameWindow;
 import javax.swing.*;
 import java.awt.*;
 
-public class GamePanel extends JPanel
+public class GamePanel extends JPanel implements ChangeListener
 {
     JPanel colorSelector;
     JSlider rSlider,gSlider,bSlider;
@@ -12,13 +12,24 @@ public class GamePanel extends JPanel
     private int r,g,b;
     public GamePanel()
     {
-       colorSelector  = new JPanel();
-       rSlider = new JSlider(0,255,0);
-       gSlider= new JSlider(0,255,0);
-       bSlider= new JSlider(0,255,0);
-       r = rSlider.getValue();
-       g = gSlider.getValue();
-       b = bSlider.getValue();
+        colorSelector  = new JPanel();
+        rSlider = new JSlider(0,255,0);
+        rSlider.setPreferredSize(new Dimension(255,50));
+        
+        gSlider= new JSlider(0,255,0);
+        gSlider.setPreferredSize(new Dimension(255,50));
+        
+        bSlider= new JSlider(0,255,0);
+        bSlider.setPreferredSize(new Dimension(255,50));
+        
+        r = rSlider.getValue();
+        g = gSlider.getValue();
+        b = bSlider.getValue();
+
+        colorSelector.setLayout(null);
+        colorSelector.add(rSlider);
+        colorSelector.add(gSlider);
+        colorSelector.add(bSlider);
 
     }
 
@@ -27,5 +38,11 @@ public class GamePanel extends JPanel
     {
         super.paintComponent(g);
         g.fillRect(100,150,100,100);
+    }
+
+    @Override
+    public void sateChanged(ChangeEvent e)
+    {
+        
     }
 }
