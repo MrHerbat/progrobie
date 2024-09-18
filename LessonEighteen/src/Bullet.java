47b50fff0912f61360fbe40a4a55bd1f2ec8879a
MrHerbat <kocirel206@gmail.com>
2024-09-18 09:33:19 +0200

    continued LessonEighteen

---
+++ LessonEighteen/src/Bullet.java
@@ -0,0 +1,18 @@
+public class Bullet
+{
+    double diameter;
+    double weight;
+    double height;
+    String contour;
+    String face;
+    String materialType;
+
+    public Bullet(double diameter, double weight, double height, String contour, String face, String materialType) {
+        this.diameter = diameter;
+        this.weight = weight;
+        this.height = height;
+        this.contour = contour;
+        this.face = face;
+        this.materialType = materialType;
+    }
+}
--- LessonEighteen/src/Main.java
+++ LessonEighteen/src/Main.java
@@ -8,23 +8,27 @@ public class Main {
         Person tank1member1 = new Person("Thomas","Commander");
         Person tank1member2 = new Person("Jason","Driver");
         Person tank1member3 = new Person("Mike","Loader");
+        Person tank1member4 = new Person("Jozef","Loader");
 
         Person tank2member1 = new Person("Hans","Commander");
         Person tank2member2 = new Person("Eric","Driver");
         Person tank2member3 = new Person("Lucas","Loader");
+        Person tank2member4 = new Person("Jozef","Loader");
 
         Person tank3member1 = new Person("Iosif","Commander");
         Person tank3member2 = new Person("Walentin","Driver");
         Person tank3member3 = new Person("Nazar","Loader");
+        Person tank3member4 = new Person("Jozef","Loader");
 
         Person tank4member1 = new Person("Adam","Commander");
         Person tank4member2 = new Person("Tomasz","Driver");
         Person tank4member3 = new Person("Jozef","Loader");
+        Person tank4member4 = new Person("Jozef","Loader");
 
         Tank tank1 = new Tank("M4 Sherman", "USA");
+        tank1.crew.add(tank1member1);
         tank1.crew.add(tank1member2);
         tank1.crew.add(tank1member3);
-        tank1.crew.add(tank1member1);
         Tank tank2 = new Tank("PanzerII", "Germany");
         tank2.crew.add(tank2member1);
         tank2.crew.add(tank2member2);
@@ -50,6 +54,26 @@ public class Main {
                 System.out.println("Name: "+person.name+" | Job: "+person.job);
             }
             System.out.println();
+        }
+//        for (int i = 0; i < tankList.size(); i++) {
+//            tankList.get(i).crew.removeFirst();
+//        }
+
+
+        ArrayList<Bullet> ammunitionMagazine = new ArrayList<>(100000);
+
+        Bullet bullet = new Bullet(7.62d,154,38.7d,"boat tail","spear tip","coppr/steel");
+
+        for (int i = 0; i < 100000; i++) {
+            if(ammunitionMagazine.isEmpty())
+            {
+                for (int j = 0; j < 100000; j++) {
+                    ammunitionMagazine.add(bullet);
+                }
+            } else{
+                ammunitionMagazine.clear();
+            }
+
         }
     }
 }
