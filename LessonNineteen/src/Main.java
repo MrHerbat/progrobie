04140b3b03803a4b3ecb82d5cea463deb7a28ba7
MrHerbat <kocirel206@gmail.com>
2024-09-18 10:50:50 +0200

    Lesson Nineteen almost completed

--- LessonNineteen/src/Main.java
+++ LessonNineteen/src/Main.java
@@ -1,4 +1,6 @@
 import java.util.ArrayList;
+import java.util.Collection;
+import java.util.Collections;
 import java.util.Iterator;
 
 //TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
@@ -22,5 +24,50 @@ public class Main {
             }
         }
         System.out.println(fruitBasket);
+
+
+        ArrayList<String> uczniowie = new ArrayList<>();
+        uczniowie.add("Leśniak");
+        uczniowie.add("Sulewski");
+        uczniowie.add("Bulanda");
+        uczniowie.add("Suchan");
+        uczniowie.add("Sanowski");
+        uczniowie.add("Sławiński");
+        uczniowie.add("Rosiek");
+        uczniowie.add("Machnik");
+        uczniowie.add("Mirecki");
+        uczniowie.add("Owsiński");
+        uczniowie.add("Sikora");
+        uczniowie.add("Machinko");
+        uczniowie.add("Mastaj");
+        uczniowie.add("Pielak");
+        uczniowie.add("Sańka");
+
+        System.out.println(uczniowie);
+
+        Collections.sort(uczniowie);
+
+        System.out.println(uczniowie);
+
+        ArrayList<Integer> number1 = new ArrayList<>();
+        ArrayList<Integer> number2 = new ArrayList<>();
+
+        int temp = 0;
+        for(int i = 0;i<10;i++)
+        {
+            number1.add(i);
+        }
+
+        for (int i = 0; i < 10; i++) {
+            temp = (int)(Math.random()*(10));
+            number2.add(temp);
+        }
+
+        System.out.println(number1);
+        System.out.println(number2);
+        number1.retainAll(number2);
+        Collections.sort(number1);
+        System.out.println(number1);
+
     }
 }
