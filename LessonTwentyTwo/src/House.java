87a7d474cc9c9dc4f621b1ca77baf145f53df7ce
MrHerbat <kocirel206@gmail.com>
2024-11-12 15:39:51 +0100

    fixed(?) LessonTwentyTwo

--- LessonTwentyTwo/src/House.java
+++ LessonTwentyTwo/src/House.java
@@ -5,13 +5,22 @@ public class House
 {
     public class Kitchen
     {
+        ArrayList<Door> kitchenDoors = new ArrayList<>();
         public void fillTheKitchen()
         {
             int a;
             Scanner scan = new Scanner(System.in);
             System.out.print("Enter amount of doors: ");
             a = scan.nextInt();
-            for (int i = 0; i < a; i++) doors.add(new Door());
+            for (int i = 0; i < a; i++) kitchenDoors.add(new Door());
+            allDoors+=kitchenDoors.size();
+        }
+        public void openAllDoors()
+        {
+            for (int i = 0; i < kitchenDoors.size(); i++) {
+                kitchenDoors.get(i).openTheDoor();
+            }
+            System.out.println("All doors in the kitchen have been opened");
         }
     }
     public class Bathroom
@@ -36,7 +45,7 @@ public class House
         {
 
         }
-
+        ArrayList<Door> bathroomDoors = new ArrayList<>();
         ArrayList<Shower> showers = new ArrayList<>();
         ArrayList<Bathtub> bathtubs = new ArrayList<>();
         ArrayList<Sink> sinks = new ArrayList<>();
@@ -49,6 +58,13 @@ public class House
             this.sinks = sinks;
             this.washers = washers;
             this.cabinets = cabinets;
+        }
+        public void openAllDoors()
+        {
+            for (int i = 0; i < bathroomDoors.size(); i++) {
+                bathroomDoors.get(i).openTheDoor();
+            }
+            System.out.println("All doors in the bathroom have been opened");
         }
 
         public String toString()
@@ -65,7 +81,7 @@ public class House
             Scanner scan = new Scanner(System.in);
             System.out.print("Enter amount of doors: ");
             a = scan.nextInt();
-            for (int i = 0; i < a; i++) doors.add(new Door());
+            for (int i = 0; i < a; i++) bathroomDoors.add(new Door());
             System.out.print("Enter amount of showers: ");
             a = scan.nextInt();
             for (int i = 0; i < a; i++) showers.add(new Shower());
@@ -81,6 +97,7 @@ public class House
             System.out.print("Enter amount of cabinets: ");
             a = scan.nextInt();
             for (int i = 0; i < a; i++) cabinets.add(new Cabinet());
+            allDoors+=bathroomDoors.size();
         }
     }
     public class Window
@@ -89,24 +106,32 @@ public class House
     }
     public class Room
     {
+        ArrayList<Door> roomDoors = new ArrayList<>();
         public void fillTheRoom()
         {
             int a;
             Scanner scan = new Scanner(System.in);
             System.out.print("Enter amount of doors: ");
             a = scan.nextInt();
-            for (int i = 0; i < a; i++) doors.add(new Door());
+            for (int i = 0; i < a; i++) roomDoors.add(new Door());
+            allDoors+=roomDoors.size();
+        }
+        public void openAllDoors()
+        {
+            for (int i = 0; i < roomDoors.size(); i++) {
+                roomDoors.get(i).openTheDoor();
+            }
+            System.out.println("All doors in the bathroom have been opened");
         }
     }
     public class Door
     {
         public void openTheDoor()
         {
-            System.out.println("Doors are opened!");
+            System.out.println("Door has been opened!");
         }
     }
-
-    ArrayList<Door> doors = new ArrayList<>();
+    int allDoors;
     ArrayList<Window> windows = new ArrayList<>();
     ArrayList<Room> rooms = new ArrayList<>();
     ArrayList<Kitchen> kitchens = new ArrayList<>();
@@ -114,7 +139,7 @@ public class House
 
 
     public House() {
-        this.doors = doors;
+        this.allDoors = allDoors;
         this.windows = windows;
         this.rooms = rooms;
         this.kitchens = kitchens;
@@ -149,15 +174,9 @@ public class House
     }
     public String toString()
     {
-        return ("This house contains: \n - "+doors.size()+" door/s \n - "+windows.size()+" window/s \n - "
+        return ("This house contains: \n - "+allDoors+" door/s \n - "+windows.size()+" window/s \n - "
                 +windows.size()+" room/s \n - "+kitchens.size()+" kitchen/s \n - "
                 +bathrooms.size()+" bathroom/s");
-    }
-    public void openAllDoors()
-    {
-        for (int i = 0; i < doors.size(); i++) {
-            doors.get(i).openTheDoor();
-        }
     }
 
 }
